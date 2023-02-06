ad22e0f22904ea5c553ecfde5cc1d143c7d336c4
yurash <yurash.bogdan@gmail.com>
2023-02-06 16:41:57 +0200

    Implement add func

---
+++ src/TestClass.java
@@ -0,0 +1,12 @@
+public class TestClass {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        System.out.println(add(a, b));
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+}
